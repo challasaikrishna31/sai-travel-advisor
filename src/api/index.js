567b5611a803f3b3ba879923e0caf3b6cf82ec28
skchalla <skchalla@DESKTOP-NU9V6HJ>
2022-07-19 11:58:21 +0530

    RAPID API key plcaed again in env file

--- src/api/index.js
+++ src/api/index.js
@@ -16,8 +16,7 @@ export const getPlacesData = async (type, sw, ne) => {
           tr_longitude: ne.lng,
         },
         headers: {
-          'X-RapidAPI-Key':
-            '21478db35cmsh06c1598b9345597p117be9jsn5ffa8cd835f2',
+          'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
           'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
         },
       }
@@ -46,8 +45,7 @@ export const getWeatherData = async (lat, lng) => {
           lon: lng,
         },
         headers: {
-          'X-RapidAPI-Key':
-            '21478db35cmsh06c1598b9345597p117be9jsn5ffa8cd835f2',
+          'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
           'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
         },
       }
